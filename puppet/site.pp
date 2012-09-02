6fbc2eeb9aa5199163fe27ab63b65200333adb61
Thomas Steur <thomas.steur@mayflower.de>
2012-09-02 11:30:00 +0200

    change owner of docroot

--- puppet/site.pp
+++ puppet/site.pp
@@ -13,6 +13,15 @@ class { 'piwik':
   svn_password  => $svn_password,
 }
 
+exec { "add_vagrant_to_wwwdata":
+  command => "sudo adduser vagrant www-data",
+}
+
+exec { "chown_docroot":
+  command => "sudo chown -R vagrant:www-data /var/www/piwik/",
+  require  => Class['piwik'],
+}
+
 piwik::apache { 'apache.piwik':
   port     => 80,
   docroot  => '/var/www/piwik',
