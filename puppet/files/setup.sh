38e5e5e927ed1d5642192edab3941101100364d8
Thomas Steur <thomas.steur@gmail.com>
2014-10-30 10:46:05 +1300

    added instructions to move mysql data dir to RAM and also added a README so whenever logs into the AWS instance there is a README file

--- puppet/files/setup.sh
+++ puppet/files/setup.sh
@@ -4,9 +4,18 @@ sudo rm /etc/php5/apache2/conf.d/20-xdebug.ini
 sudo rm /etc/php5/fpm/conf.d/20-xdebug.ini
 
 ## Prepare configs
+## cp README /home/ubuntu/README
 ## cp www/piwik/config/config.ini.php /home/ubuntu/www/piwik/config/config.ini.php
 ## cp etc/mysql/my.cnf /etc/mysql/my.cnf
 ## todo update apparmor for load infile support
+
+## only do this on AWS or only on a server where you only want to execute tests
+## sudo mkdir /tmp/ramdisk
+## sudo mount -t tmpfs -o size=1536M tmpfs /tmp/ramdisk/
+## sudo mv /var/lib/mysql /tmp/ramdisk/mysql
+## sudo ln -s /tmp/ramdisk/mysql/ /var/lib/mysql
+## sudo chmod -R 770 /var/lib/mysql
+## sudo chown -R ubuntu:ubuntu /var/lib/mysql
 
 ## Restart services
 sudo service mysql restart
